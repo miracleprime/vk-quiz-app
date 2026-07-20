3d5536a85b28f7d9b83fc8da380b2d34759e348e
miracleprime <k.rublev777@gmail.com>
2026-07-20 14:38:04 +0500

    Final project fixes

--- database.js
+++ database.js
@@ -1,7 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(__dirname, 'db', 'database.sqlite');
+const dbDir = path.join(__dirname, 'db');
+
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
+const dbPath = path.join(dbDir, 'database.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 function run(sql, params = []) {
